da58ab021485c5f2939984cf4bda0f58f43950c2
bxq2011hust <bxq2011hust@qq.com>
2021-10-27 16:27:18 +0800

    fix scan get uncommitted data

--- src/kv/Scanner.cc
+++ src/kv/Scanner.cc
@@ -2,141 +2,144 @@
 
 namespace pingcap
 {
-namespace kv
-{
-void Scanner::next()
-{
-    Backoffer bo(scanMaxBackoff);
-    if (!valid)
+    namespace kv
     {
-        throw Exception("the scanner is invalid", LogicalError);
-    }
-
-    for (;;)
-    {
-        idx++;
-        if (idx >= cache.size())
+        void Scanner::next()
         {
-            if (eof)
+            Backoffer bo(scanMaxBackoff);
+            if (!valid)
             {
-                valid = false;
+                throw Exception("the scanner is invalid", LogicalError);
+            }
+
+            for (;;)
+            {
+                idx++;
+                if (idx >= cache.size())
+                {
+                    if (eof)
+                    {
+                        valid = false;
+                        return;
+                    }
+                    getData(bo);
+                    if (idx >= cache.size())
+                    {
+                        continue;
+                    }
+                }
+
+                auto &current = cache[idx];
+                if (end_key.size() > 0 && current.key() >= end_key)
+                {
+                    eof = true;
+                    valid = false;
+                }
+
+                if (current.has_error())
+                {
+                    if (resolveCurrentLock(bo, current))
+                    {
+                        continue;
+                    }
+                }
                 return;
             }
-            getData(bo);
-            if (idx >= cache.size())
+        }
+
+        bool Scanner::resolveCurrentLock(pingcap::kv::Backoffer &bo, kvrpcpb::KvPair &current)
+        {
+            auto value = snap.Get(bo, current.key());
+            current.set_allocated_error(nullptr);
+            current.set_value(value);
+            return value.empty();
+        }
+
+        void Scanner::getData(Backoffer &bo)
+        {
+            log->trace("get data for scanner");
+            for (;;)
             {
-                continue;
+                auto loc = snap.cluster->region_cache->locateKey(bo, next_start_key);
+                auto req_end_key = end_key;
+                if (req_end_key.size() > 0 && loc.end_key.size() > 0 && loc.end_key < req_end_key)
+                    req_end_key = loc.end_key;
+
+                auto regionClient = RegionClient(snap.cluster, loc.region);
+                auto request = std::make_shared<kvrpcpb::ScanRequest>();
+                request->set_start_key(next_start_key);
+                request->set_end_key(req_end_key);
+                request->set_limit(batch);
+                request->set_version(snap.version);
+                // FISCO-BCOS only need keys
+                request->set_key_only(true);
+
+                auto context = request->mutable_context();
+                context->set_priority(::kvrpcpb::Normal);
+                context->set_not_fill_cache(false);
+
+                std::shared_ptr<kvrpcpb::ScanResponse> response;
+                try
+                {
+                    response = regionClient.sendReqToRegion(bo, request);
+                }
+                catch (Exception &e)
+                {
+                    bo.backoff(boRegionMiss, e);
+                    continue;
+                }
+
+                // TODO Check safe point.
+
+                // TiKV will only return locked keys if there is response level error
+                if (response->has_error())
+                {
+                    auto lock = extractLockFromKeyErr(response->error());
+                    std::vector<LockPtr> locks{lock};
+                    std::vector<uint64_t> pushed{};
+                    auto ms_before_expired = snap.cluster->lock_resolver->ResolveLocks(bo, snap.version, locks, pushed);
+                    if (ms_before_expired > 0)
+                    {
+                        bo.backoffWithMaxSleep(
+                            BackoffType::boTxnLockFast, ms_before_expired, Exception("key is locked during scanning", ErrorCodes::LockError));
+                    }
+                    continue;
+                }
+
+                int pairs_size = response->pairs_size();
+                idx = 0;
+                cache.clear();
+                for (int i = 0; i < pairs_size; i++)
+                {
+                    auto current = response->pairs(i);
+                    if (current.has_error())
+                    {
+                        auto lock = extractLockFromKeyErr(current.error());
+                        current.set_key(lock->key);
+                    }
+                    cache.push_back(current);
+                }
+
+                log->trace("get pair size: " + std::to_string(pairs_size));
+
+                if (pairs_size < batch)
+                {
+                    next_start_key = loc.end_key;
+
+                    // If the end key is empty, it infers this region is last and should stop scan.
+                    if (loc.end_key.size() == 0 || (next_start_key) >= end_key)
+                    {
+                        eof = true;
+                    }
+
+                    return;
+                }
+
+                auto lastKey = cache.back();
+                next_start_key = alphabeticalNext(lastKey.key());
+                log->trace("scan next key: " + next_start_key);
+                return;
             }
         }
-
-        auto & current = cache[idx];
-        if (end_key.size() > 0 && current.key() >= end_key)
-        {
-            eof = true;
-            valid = false;
-        }
-
-        if (current.has_error())
-        {
-            resolveCurrentLock(bo, current);
-        }
-        return;
-    }
-}
-
-void Scanner::resolveCurrentLock(pingcap::kv::Backoffer & bo, kvrpcpb::KvPair & current)
-{
-    auto value = snap.Get(bo, current.key());
-    current.set_allocated_error(nullptr);
-    current.set_value(value);
-}
-
-void Scanner::getData(Backoffer & bo)
-{
-    log->trace("get data for scanner");
-    for (;;)
-    {
-        auto loc = snap.cluster->region_cache->locateKey(bo, next_start_key);
-        auto req_end_key = end_key;
-        if (req_end_key.size() > 0 && loc.end_key.size() > 0 && loc.end_key < req_end_key)
-            req_end_key = loc.end_key;
-
-
-        auto regionClient = RegionClient(snap.cluster, loc.region);
-        auto request = std::make_shared<kvrpcpb::ScanRequest>();
-        request->set_start_key(next_start_key);
-        request->set_end_key(req_end_key);
-        request->set_limit(batch);
-        request->set_version(snap.version);
-        // FISCO-BCOS only need keys
-        request->set_key_only(true);
-
-        auto context = request->mutable_context();
-        context->set_priority(::kvrpcpb::Normal);
-        context->set_not_fill_cache(false);
-
-        std::shared_ptr<kvrpcpb::ScanResponse> response;
-        try
-        {
-            response = regionClient.sendReqToRegion(bo, request);
-        }
-        catch (Exception & e)
-        {
-            bo.backoff(boRegionMiss, e);
-            continue;
-        }
-
-        // TODO Check safe point.
-
-        // TiKV will only return locked keys if there is response level error
-        if (response->has_error())
-        {
-            auto lock = extractLockFromKeyErr(response->error());
-            std::vector<LockPtr> locks{lock};
-            std::vector<uint64_t> pushed{};
-            auto ms_before_expired = snap.cluster->lock_resolver->ResolveLocks(bo, snap.version, locks, pushed);
-            if (ms_before_expired > 0)
-            {
-                bo.backoffWithMaxSleep(
-                    BackoffType::boTxnLockFast, ms_before_expired, Exception("key is locked during scanning", ErrorCodes::LockError));
-            }
-            continue;
-        }
-
-        int pairs_size = response->pairs_size();
-        idx = 0;
-        cache.clear();
-        for (int i = 0; i < pairs_size; i++)
-        {
-            auto current = response->pairs(i);
-            if (current.has_error())
-            {
-                auto lock = extractLockFromKeyErr(current.error());
-                current.set_key(lock->key);
-            }
-            cache.push_back(current);
-        }
-
-        log->trace("get pair size: " + std::to_string(pairs_size));
-
-        if (pairs_size < batch)
-        {
-            next_start_key = loc.end_key;
-
-            // If the end key is empty, it infers this region is last and should stop scan.
-            if (loc.end_key.size() == 0 || (next_start_key) >= end_key)
-            {
-                eof = true;
-            }
-
-            return;
-        }
-
-        auto lastKey = cache.back();
-        next_start_key = alphabeticalNext(lastKey.key());
-        log->trace("scan next key: " + next_start_key);
-        return;
-    }
-}
-} // namespace kv
+    } // namespace kv
 } // namespace pingcap
